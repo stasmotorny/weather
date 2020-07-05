d0dc881de7a1bc9ca79fe76ec67cdf8fd2752a9f
Stas <stasmotorny@gmail.com>
2020-07-05 20:40:39 +0300

    Updates

---
+++ precache-manifest.076cf3c68f1a208a8b2b5c2d23f72b37.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9650a3c638b7d9ca7e55970728cf848d",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "ce969cd52b3ef8110ae8",
+    "url": "/weather/static/css/main.e5f1bbaf.chunk.css"
+  },
+  {
+    "revision": "170a26ab90daceed570d",
+    "url": "/weather/static/js/2.61136f7c.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/weather/static/js/2.61136f7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce969cd52b3ef8110ae8",
+    "url": "/weather/static/js/main.714c39b0.chunk.js"
+  },
+  {
+    "revision": "b7586fcaead62fc35b8b",
+    "url": "/weather/static/js/runtime-main.d777cdb5.js"
+  },
+  {
+    "revision": "db5b06734cc7b1d106aa6d65d580cd0c",
+    "url": "/weather/static/media/OvercastClouds.db5b0673.svg"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/weather/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "f09c68ec8ce4ff6138b41ac5909dabc3",
+    "url": "/weather/static/media/background.f09c68ec.jpg"
+  },
+  {
+    "revision": "dd7a0be920985b10a42c8e8a9a35ca46",
+    "url": "/weather/static/media/barometer.dd7a0be9.svg"
+  },
+  {
+    "revision": "347a5204ee493a8329b81e631af35906",
+    "url": "/weather/static/media/clear.347a5204.svg"
+  },
+  {
+    "revision": "2e0b4ef63c22d0f201addef3c0cd9143",
+    "url": "/weather/static/media/clouds.2e0b4ef6.svg"
+  },
+  {
+    "revision": "89d8703b2099c02873e7c7061c877f77",
+    "url": "/weather/static/media/fewClouds.89d8703b.svg"
+  },
+  {
+    "revision": "c14731bb329dee742b66449f8a8c9a62",
+    "url": "/weather/static/media/humidity.c14731bb.svg"
+  },
+  {
+    "revision": "9b7433331d0ef6d6cef6367888a85a67",
+    "url": "/weather/static/media/rain.9b743333.svg"
+  },
+  {
+    "revision": "7647ba3da710f4f8d59d8c85132e422e",
+    "url": "/weather/static/media/snow.7647ba3d.svg"
+  },
+  {
+    "revision": "99a877c728c2aaf211b98410cb91357a",
+    "url": "/weather/static/media/thunderstorm.99a877c7.svg"
+  },
+  {
+    "revision": "c04e2d5d5bca8423264ec85e4f1f6372",
+    "url": "/weather/static/media/wind.c04e2d5d.svg"
+  }
+]);
