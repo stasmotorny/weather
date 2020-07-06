0b81471efab70eb678549b219ad95dc0682c0922
Stas <stasmotorny@gmail.com>
2020-07-06 23:01:40 +0300

    works on local server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '../', 'public');
+const publicPath = path.join(__dirname,  '../', 'build');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
@@ -9,4 +9,5 @@ app.get('*', (req, res) => {
 });
 app.listen(port, () => {
     console.log('Server is up!');
+    console.log(path.join(publicPath, 'index.html'))
 });
